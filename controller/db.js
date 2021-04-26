3ae19ac52743309d4a9727ef940d7042622dc42f
Sandro Carvalho <sandrinho9797@outlook.com>
2021-04-26 13:11:30 -0300

    config db

--- controller/db.js
+++ controller/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 require('dotenv/config')
 
 //Connect DB
-const sequelize = new Sequelize(process.env.DB_NAME,'root', process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME,'teste_hvex', process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
 })
